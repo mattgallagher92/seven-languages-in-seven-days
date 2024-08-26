ec275419408fb658cb30b222e8166dd8fec10dae
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-08-26 16:22:05 +0100

    Read alternative word list from file

--- scala/censor.scala
+++ scala/censor.scala
@@ -1,6 +1,17 @@
 // This problem is very poorly defined - what exactly does the author want me to do?
 trait Censor {
-  val appropriateWords = Map("Shoot" -> "Pucky", "Darn" -> "Beans")
+  val appropriateWords = {
+    val m = new scala.collection.mutable.HashMap[String, String]
+    val pairs =
+      scala.io.Source.fromFile("./alternativeWords.txt")
+        .getLines()
+        .filter(l => !l.isEmpty)
+        .foreach(l => {
+          val parts = l.split(", ")
+          m += parts(0) -> parts(1)
+        })
+    m
+  }
 
   def censor(s: String) = appropriateWords.foldLeft(s)((acc, kvp) => acc.replaceAll(kvp._1, kvp._2))
 }
