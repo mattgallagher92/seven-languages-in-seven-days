fba6e1d57948ca8a26297d2686e282320bd8e827
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-10-08 22:44:46 +0100

    Count links

--- scala/sizer.scala
+++ scala/sizer.scala
@@ -3,7 +3,11 @@ import scala.actors._
 import Actor._
 
 object PageLoader {
-   def getPageSize(url : String) = Source.fromURL(url).mkString.length
+  def getPageSizeAndLinkCount(url : String) = {
+    val sourceString = Source.fromURL(url).mkString
+    val linkCount = "</a>".r.findAllIn(sourceString).length
+    (sourceString.length, linkCount)
+  }
 }
 
 val urls = List("http://www.google.co.uk/",
@@ -17,35 +21,35 @@ def timeMethod(method: () => Unit) = {
   println("Method took " + (end - start)/1000000000.0 + " seconds.")
 }
 
-def getPageSizeSequentially() = {
+def getPageSizeAndLinkCountSequentially() = {
   for(url <- urls) {
-    println("Size for " + url + ": " + PageLoader.getPageSize(url))
+    println("Size and link count for " + url + ": " + PageLoader.getPageSizeAndLinkCount(url))
   }
 }
 
-def getPageSizeConcurrently() = {
+def getPageSizeAndLinkCountConcurrently() = {
   // `self` is apparently an implicit actor associated with the current execution context.
   val caller = self
 
   for(url <- urls) {
-    // Need to use actors here to avoid blocking while waiting for getPageSize to return.
-    actor { caller ! (url, PageLoader.getPageSize(url)) }
+    // Need to use actors here to avoid blocking while waiting for getPageSizeAndLinkCount to return.
+    actor { caller ! (url, PageLoader.getPageSizeAndLinkCount(url)) }
   }
 
   for(i <- 1 to urls.size) {
     receive {
       case (url, size) =>
-        println("Size for " + url + ": " + size)
+        println("Size and link count for " + url + ": " + size)
     }
   }
 }
 
 // There seems to be some speed up after the first fetch.
 println("Warming up...")
-timeMethod { getPageSizeConcurrently }
+timeMethod { getPageSizeAndLinkCountConcurrently }
 
 println("Sequential run:")
-timeMethod { getPageSizeSequentially }
+timeMethod { getPageSizeAndLinkCountSequentially }
 
 println("Concurrent run")
-timeMethod { getPageSizeConcurrently }
+timeMethod { getPageSizeAndLinkCountConcurrently }
