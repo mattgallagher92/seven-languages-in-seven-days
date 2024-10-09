6657d09538c2ac4e18ea8afcfb20842e7235e6bb
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-10-09 08:55:33 +0100

    Extract and print links

--- scala/sizer.scala
+++ scala/sizer.scala
@@ -3,16 +3,17 @@ import scala.actors._
 import Actor._
 
 object PageLoader {
-  def getPageSizeAndLinkCount(url : String) = {
+  def getPageSizeAndLinks(url : String) = {
     val sourceString = Source.fromURL(url).mkString
     val linkCount = "</a>".r.findAllIn(sourceString).length
-    (sourceString.length, linkCount)
+    val linkExtractionRegex = "<a[^>]+?href=\"(.+?)\"".r
+    val links = linkExtractionRegex.findAllIn(sourceString).matchData.map(_.group(1)).toArray
+    (sourceString.length, links)
   }
 }
 
-val urls = List("http://www.google.co.uk/",
-               "http://www.google.com/",
-               "http://www.google.ca/")
+// Other common sites aren't working, I suspect due to HTTPS redirects.
+val urls = List("http://www.google.co.uk/", "http://www.google.com/", "http://www.google.ca/")
 
 def timeMethod(method: () => Unit) = {
   val start = System.nanoTime
@@ -21,35 +22,45 @@ def timeMethod(method: () => Unit) = {
   println("Method took " + (end - start)/1000000000.0 + " seconds.")
 }
 
-def getPageSizeAndLinkCountSequentially() = {
+def createMessage(url: String, size : Int, links : Array[String]) = {
+  val linkLines = links.mkString("\n    ")
+  s"""
+  Size of $url: $size
+  Links in $url:
+    $linkLines
+  """
+}
+
+def getPageSizeAndLinksSequentially() = {
   for(url <- urls) {
-    println("Size and link count for " + url + ": " + PageLoader.getPageSizeAndLinkCount(url))
+    val (size, links) = PageLoader.getPageSizeAndLinks(url)
+    println(createMessage(url, size, links))
   }
 }
 
-def getPageSizeAndLinkCountConcurrently() = {
+def getPageSizeAndLinksConcurrently() = {
   // `self` is apparently an implicit actor associated with the current execution context.
   val caller = self
 
   for(url <- urls) {
-    // Need to use actors here to avoid blocking while waiting for getPageSizeAndLinkCount to return.
-    actor { caller ! (url, PageLoader.getPageSizeAndLinkCount(url)) }
+    // Need to use actors here to avoid blocking while waiting for getPageSizeAndLinks to return.
+    actor { caller ! (url, PageLoader.getPageSizeAndLinks(url)) }
   }
 
   for(i <- 1 to urls.size) {
     receive {
-      case (url, size) =>
-        println("Size and link count for " + url + ": " + size)
+      case (url : String, (size : Int, links : Array[String])) =>
+        println(createMessage(url, size, links))
     }
   }
 }
 
 // There seems to be some speed up after the first fetch.
 println("Warming up...")
-timeMethod { getPageSizeAndLinkCountConcurrently }
+timeMethod { getPageSizeAndLinksConcurrently }
 
 println("Sequential run:")
-timeMethod { getPageSizeAndLinkCountSequentially }
+timeMethod { getPageSizeAndLinksSequentially }
 
 println("Concurrent run")
-timeMethod { getPageSizeAndLinkCountConcurrently }
+timeMethod { getPageSizeAndLinksConcurrently }
