0e752642fbc1029e7e8cbf2db7403764d9045eab
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-10-21 19:21:29 +0100

    Reformat Erlang code (elp cares less than erlang_ls)

--- erlang/day_1.erl
+++ erlang/day_1.erl
@@ -4,31 +4,22 @@
 -export([count_to_ten/0]).
 -export([print_result/1]).
 
-num_words("") ->
-        0;
+num_words("") -> 0;
 % Simplifying assumption: String contains words separated by single spaces.
-num_words(String) ->
-        words(String, 1).
+num_words(String) -> words(String, 1).
 
-words("", Num) ->
-        Num;
+words("", Num) -> Num;
 % 32 is the Unicode code point for a space.
-words([32 | T], Num) ->
-        words(T, Num + 1);
-words([_ | T], Num) ->
-        words(T, Num).
+words([32 | T], Num) -> words(T, Num + 1);
+words([_ | T], Num) -> words(T, Num).
 
-count_to_ten() ->
-        count_to_ten(1).
+count_to_ten() -> count_to_ten(1).
 
-count_to_ten(11) ->
-        % TODO: how to avoid returning something here?
-        [];
+% TODO: how to avoid returning something here?
+count_to_ten(11) -> [];
 count_to_ten(X) ->
-        io:format("~p~n", [X]),
-        count_to_ten(X + 1).
+    io:format("~p~n", [X]),
+    count_to_ten(X + 1).
 
-print_result(success) ->
-        "success";
-print_result({error, Message}) ->
-        "error: " ++ Message.
+print_result(success) -> "success";
+print_result({error, Message}) -> "error: " ++ Message.
--- erlang/day_2.erl
+++ erlang/day_2.erl
@@ -4,55 +4,51 @@
 -export([subtotals/1]).
 -export([tic_tac_toe_result/1]).
 
-value_for_keyword(_, []) ->
-        not_found;
-value_for_keyword(Keyword, [{Keyword, V} | _]) ->
-        V;
-value_for_keyword(Keyword, [_ | T]) ->
-        value_for_keyword(Keyword, T).
+value_for_keyword(_, []) -> not_found;
+value_for_keyword(Keyword, [{Keyword, V} | _]) -> V;
+value_for_keyword(Keyword, [_ | T]) -> value_for_keyword(Keyword, T).
 
 %day_2:value_for_keyword(dog, [{mouse, "Squeak"}, {cat, "Meow"}]).
 %not_found
 %day_2:value_for_keyword(dog, [{mouse, "Squeak"}, {cat, "Meow"}, {dog, "Woof"}]).
 %"Woof"
 
-subtotals(ShoppingList) ->
-        [{Item, Quantity * Price} || {Item, Quantity, Price} <- ShoppingList].
+subtotals(ShoppingList) -> [{Item, Quantity * Price} || {Item, Quantity, Price} <- ShoppingList].
 
 % subtotals([{onions, 3, 0.50}, {tomatoes, 2, 0.59}, {mushrooms, 10, 0.12}]).
 % [{onions,1.5},{tomatoes,1.18},{mushrooms,1.2}]
 
 % Expects x and o as player marks. Anything else, e.g. e, can be used for empty cells.
 tic_tac_toe_result(Input) ->
-        {Tl, Tm, Tr, Ml, Mm, Mr, Bl, Bm, Br} = Input,
+    {Tl, Tm, Tr, Ml, Mm, Mr, Bl, Bm, Br} = Input,
 
-        LCol = {Tl, Ml, Bl},
-        MCol = {Tm, Mm, Bm},
-        RCol = {Tr, Mr, Br},
+    LCol = {Tl, Ml, Bl},
+    MCol = {Tm, Mm, Bm},
+    RCol = {Tr, Mr, Br},
 
-        TRow = {Tl, Tm, Tr},
-        MRow = {Ml, Mm, Mr},
-        BRow = {Bl, Bm, Br},
+    TRow = {Tl, Tm, Tr},
+    MRow = {Ml, Mm, Mr},
+    BRow = {Bl, Bm, Br},
 
-        TlToBrDiag = {Tl, Mm, Br},
-        TrToBlDiag = {Tr, Mm, Bl},
+    TlToBrDiag = {Tl, Mm, Br},
+    TrToBlDiag = {Tr, Mm, Bl},
 
-        Lines = [LCol, MCol, RCol, TRow, MRow, BRow, TlToBrDiag, TrToBlDiag],
+    Lines = [LCol, MCol, RCol, TRow, MRow, BRow, TlToBrDiag, TrToBlDiag],
 
-        XHasWon = lists:member({x, x, x}, Lines),
-        OHasWon = lists:member({o, o, o}, Lines),
-        SomeCellsAreEmpty = lists:any(fun(S) -> S =/= x andalso S =/= o end, tuple_to_list(Input)),
+    XHasWon = lists:member({x, x, x}, Lines),
+    OHasWon = lists:member({o, o, o}, Lines),
+    SomeCellsAreEmpty = lists:any(fun(S) -> S =/= x andalso S =/= o end, tuple_to_list(Input)),
 
-        if XHasWon ->
-                   x;
-           OHasWon ->
-                   o;
-           SomeCellsAreEmpty ->
-                   no_winner;
-           true ->
-                   % Don't get why it's "cat", that's just what the exercise says...
-                   cat
-        end.
+    if XHasWon ->
+           x;
+       OHasWon ->
+           o;
+       SomeCellsAreEmpty ->
+           no_winner;
+       true ->
+           % Don't get why it's "cat", that's just what the exercise says...
+           cat
+    end.
 
 %day_2:tic_tac_toe_result({x, o, x, o, x, e, x, e, o}).
 %x
