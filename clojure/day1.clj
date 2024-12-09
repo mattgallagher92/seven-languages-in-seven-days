30fd5eea26f71635c71ff4917dc241135bc45b4a
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-09 11:27:23 +0000

    Add Clojure day 1 ex 2

--- clojure/day1.clj
+++ clojure/day1.clj
@@ -3,3 +3,16 @@
 (defn printSize [st n] (println (str (str st " is " (if (big st n) "big." "not big.")))))
 (printSize "Elephant" 6)
 (printSize "Mouse" 6)
+
+(defn collection-type [coll]
+  (if (list? coll)
+    :list
+    (if (map? coll)
+      :map
+      (if (vector? coll)
+        :vector))))
+
+(defn printCollectionType [coll] (println (str coll " is a " (collection-type coll))))
+(printCollectionType '(1 2 3 4))
+(printCollectionType '[1 2 3 4])
+(printCollectionType '{1 2, 3 4})
