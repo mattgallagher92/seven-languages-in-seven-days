d091b5cfce4a8254b3f0e661ab07fd297c561781
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-16 21:45:27 +0000

    Add sleeping barber solution outline

---
+++ clojure/day3-sleeping-barber.clj
@@ -0,0 +1,11 @@
+; Barber behaviour:
+; - Start asleep.
+; - When woken, cut hair.
+; - When done, inform customer, call for next customer and sleep.
+
+; Cusomter behaviour:
+; - If barber's seat is empty on arrival, sit in it and wake barber.
+; - If barber's seat is occupied on arrival but there is empty chair in the waiting room, sit in it, noting number of customers ahead.
+; - If there are no available seats on arrival, leave.
+; - If next customer is called and there are no customers ahead of them, sit in barber seat and wake barber.
+; - If next customer is called and there are customers ahead of them, decrement number of customers ahead of them.
