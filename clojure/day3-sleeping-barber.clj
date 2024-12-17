e659d837802e942c787df79ec8923285cfbce4f0
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-12-17 21:19:39 +0000

    Add Clojure sleeping barbers solution

--- clojure/day3-sleeping-barber.clj
+++ clojure/day3-sleeping-barber.clj
@@ -1,11 +1,101 @@
-; Barber behaviour:
-; - Start asleep.
-; - When woken, cut hair.
-; - When done, inform customer, call for next customer and sleep.
+; Value represents whether barber is awake.
+(def barber (ref false))
+(defn wakeBarber [] (ref-set barber true))
+(defn fallAsleep [] (ref-set barber false))
+
+; Value represents customer number in chair.
+(def barberChair (ref nil))
+; TODO: consider refactor: use chairs vector rather than explicit ref for each chair.
+(def chair1 (ref nil))
+(def chair2 (ref nil))
+(def chair3 (ref nil))
+(defn numOccupiedChairs [] (count (filter identity [@chair1 @chair2 @chair3])))
+
+(def customers (ref []))
+(defn leaveImmediately [n] (alter customers #(assoc % n :uncut)))
+(defn sitInBarberChair [n] (alter customers #(assoc % n :cutting)) (ref-set barberChair n))
+(defn haveHairCut [n] (alter customers #(assoc % n :cut)))
+(defn sitInWaitingRoom [n numEarlierWaitingCustomers] (alter customers #(assoc % n numEarlierWaitingCustomers)))
+(defn moveUpInQueue [n] (alter customers #(assoc % n (dec (get @customers n)))))
+
+; Barber behaviour: if there are no waiting customers, fall asleep; otherwise invite next customer to barber chair.
+; Customer behaviour:
+; - If next customer is called and there are no customers ahead of them, sit in barber seat and wake barber.
+; - If next customer is called and there are customers ahead of them, decrement number of customers ahead of them.
+(defn callNextCustomer []
+  (if (and (not @chair1) (not @chair2) (not @chair3))
+    (fallAsleep)
+    (do
+      (if @chair1
+        (if (= (get @customers @chair1) 0)
+          (do (sitInBarberChair @chair1) (ref-set chair1 nil) (wakeBarber))
+          (moveUpInQueue @chair1))
+        nil)
+      (if @chair2
+        (if (= (get @customers @chair2) 0)
+          (do (sitInBarberChair @chair2) (ref-set chair2 nil) (wakeBarber))
+          (moveUpInQueue @chair2))
+        nil)
+      (if @chair3
+        (if (= (get @customers @chair3) 0)
+          (do (sitInBarberChair @chair3) (ref-set chair3 nil) (wakeBarber))
+          (moveUpInQueue @chair3))
+        nil))))
+
+(defn cutHair []
+  (dosync
+   (let [n @barberChair]
+     (Thread/sleep 20) (haveHairCut n) (ref-set barberChair nil) (callNextCustomer))))
 
 ; Cusomter behaviour:
 ; - If barber's seat is empty on arrival, sit in it and wake barber.
 ; - If barber's seat is occupied on arrival but there is empty chair in the waiting room, sit in it, noting number of customers ahead.
 ; - If there are no available seats on arrival, leave.
-; - If next customer is called and there are no customers ahead of them, sit in barber seat and wake barber.
-; - If next customer is called and there are customers ahead of them, decrement number of customers ahead of them.
+(defn enterBarberShop [n]
+  ; We're doing all this within a transaction. Presumably, by analogy to database transactions, this means that if the
+  ; values read from the refs change before the transaction is committed, the transaction is aborted and retried.
+  (dosync
+   (if (not @barberChair)
+     (do (sitInBarberChair n) (wakeBarber))
+     (if (not @chair1)
+       (do (sitInWaitingRoom n (numOccupiedChairs)) (ref-set chair1 n))
+       (if (not @chair2)
+         (do (sitInWaitingRoom n (numOccupiedChairs)) (ref-set chair2 n))
+         (if (not @chair3)
+           (do (sitInWaitingRoom n (numOccupiedChairs)) (ref-set chair3 n))
+           (leaveImmediately n)))))))
+
+(defn timeIntervalLessThanTenSeconds [startTimeNanos endTimeNanos]
+  (< (/ (- endTimeNanos startTimeNanos) 1000000000.0) 10))
+
+(def startTime (System/nanoTime))
+
+(def barberFuture
+  (future
+    (loop []
+      (when (timeIntervalLessThanTenSeconds startTime (System/nanoTime))
+        (if @barber
+          (cutHair)
+          ; Avoid looping hundreds of thousands of times!
+          (Thread/sleep 1))
+        (recur)))))
+
+(def customersFuture
+  (future
+    (loop [counter 0]
+      (Thread/sleep (+ 10 (rand-int 21)))
+      (future (enterBarberShop counter))
+      (when (timeIntervalLessThanTenSeconds startTime (System/nanoTime))
+        (recur (inc counter))))))
+
+@barberFuture
+@customersFuture
+
+; NOTE: only a very small number of customers have their hair cut (~70 / 500). The barber loop is taking over 100 ms on
+; average; so there's are fewer than 100 iterations. That, along with contention explains the observed numbers.
+; I don't intend to fix this for now as the actual logic and use of Clojure async primitibes seems to be working.
+(println
+ (count (filter #(= :cut %) @customers)) "customers had their hair cut,"
+ (count (filter #(= :uncut %) @customers)) "left without having their hair cut,"
+ ; Because callNextCustomer runs at the end of cutHair, there'll be a customer in barberChair (:cutting).
+ (count (filter #(or (= :cutting %) (integer? %)) @customers)) "were waiting when the shop closed.")
