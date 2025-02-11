6ebaa1f02386b16bce597c179cd2a2a66f9808f9
Matt Gallagher <mjg1992@hotmail.co.uk>
2025-02-11 22:02:16 +0000

    wip wip

--- haskell/day3Maze.hs
+++ haskell/day3Maze.hs
@@ -18,21 +18,36 @@ maze =
         [ [ U ], [ U, R ], [ U, L ] ]
     ]
 
+opposite :: Direction -> Direction
+opposite U = D
+opposite D = U
+opposite L = R
+opposite R = L
+
 move :: Direction -> (Int, Int) -> (Int, Int)
 move U (r, c) = (r-1, c)
 move D (r, c) = (r+1, c)
 move L (r, c) = (r, c-1)
 move R (r, c) = (r, c+1)
 
+tryHead xs =
+    case xs of
+        [] -> Nothing
+        (h:t) -> Just h
+
 solveInner :: [Direction] -> (Int, Int) -> (Int, Int) -> [Direction]
 solveInner dirs currCoords entranceCoords =
     if currCoords == entranceCoords
     then Just dirs
     else
-        do
-            d <- maze !! fst currCoords !! snd currCoords
-            -- TODO: don't recurse if direction is opposite to previous direction we've just come.
-            x <- solveInner (d:dirs) (move d currCoords) entranceCoords
-            return x
+        let comeFrom = opposite $ tryHead dirs in
+            do
+                d <- maze !! fst currCoords !! snd currCoords
+                return
+                    if Just d == comeFrom
+                    then Nothing
+                    else
+                        x <- solveInner (d:dirs) (move d currCoords) entranceCoords
+                        x
 
 solve = take 1 $ solveInner [] (1, 2) (0, 0)
--- haskell/thoughts-on-haskell.md
+++ haskell/thoughts-on-haskell.md
@@ -11,3 +11,7 @@
 - `.` is quite nice, but not as nice as `|>`
 - Generic map/sort/filter functions are cool.
 - Lazy evaluation with linked lists enables some quite elegant code.
+
+## Day three
+
+- Haskell's error messages are pretty inscrutable. I feel like I should be in a good place to cover the basics of use of monads given my experience with F#, but it proved challenging. What the hell does `(if ... then ... else ...)-syntax in pattern` mean?
